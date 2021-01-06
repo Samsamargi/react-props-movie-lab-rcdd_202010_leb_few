e96cbf025b0a9273e97396879aedf9b6b3309ee8
Samira Fadi El Samargi <samiraelsamargi2018@gmail.com>
2021-01-06 14:18:39 +0000

    Automatically backed up by Learn

--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -4,8 +4,15 @@ export default class CardFront extends Component {
 
   render() {
     return (
-      <div className="card-front" style={{backgroundImage: `url(${''})`}}>
+<<<<<<< HEAD
+     <div
+				className="card-front"
+				style={{ backgroundImage: `url(${this.props.poster})` }}
+			/>
+=======
+      <div className="card-front" style={{ backgroundImage: `url(${this.props.poster})` }}
       </div>
+>>>>>>> cdeece03470ba9b570611834f3db572888aed9b5
     )
   }
 }
